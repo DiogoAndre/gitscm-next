5d5be5fedd8186c1d306c10be4428ec2c8b199bf
Diogo André <diogo@regattapix.com>
2013-04-12 16:54:29 -0300

    Melhorando o Blog

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,19 +1,25 @@
-module BlogHelper
-  def list_entries
+ module BlogHelper
+  def list_posts
     path = "#{Rails.root}/app/views/blog/posts/*.*"
-    entries = []
+    posts = []
     Dir[path].each do |uri|
       file = uri.match(/^.*\/(.{10})(.*).(markdown|html)/)
-      entries << {:uri => file[0], 
+      posts << {:uri => file[0],
+                  :file => file[1]  file[2],
                   :date_published => Date.parse(file[1]),
                   :title=> file[2].titleize,
                   :slug => file[2].parameterize
                 }
     end
-    entries = entries.sort_by { |k| k[:date_published] }.reverse
+    posts = posts.sort_by { |k| k[:date_published] }.reverse
   end
 
-  def format_path(entry)
-    "/blog/#{entry[:date_published].year}/#{entry[:date_published].strftime('%m')}/#{entry[:date_published].strftime('%d')}/#{entry[:slug]}.html"
+  def format_path(post)
+    "/blog/#{post[:date_published].year}/#{post[:date_published].strftime('%m')}/#{post[:date_published].strftime('%d')}/#{post[:slug]}.html"
+  end
+
+  def preview_post(postURI)
+    post = BlogPresenter.new(postURI)
+    post.render
   end
 end
